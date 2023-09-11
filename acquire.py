c4ed1196340ee475a69128327f9e9b537a3de324
Brock Green <brock.green03@gmail.com>
2023-09-11 13:13:15 -0500

    update project repo

--- acquire.py
+++ acquire.py
@@ -13,25 +13,16 @@ def get_connection_url(db, user=env.user, host=env.host, password=env.password):
     """
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-
 def new_zillow_data():
     '''
-    This function uses the url from the get_connection_url() and reads the zillow data from the Codeup db into a dataframe.
+    This function uses the url from the get_connection_url() and reads the telco data from the Codeup db into a dataframe.
     '''
     sql_query = """
-SELECT *
-FROM properties_2017
-LEFT JOIN predictions_2017 ON properties_2017.id = predictions_2017.id
-LEFT JOIN architecturalstyletype ON properties_2017.architecturalstyletypeid = architecturalstyletype.architecturalstyletypeid
-LEFT JOIN buildingclasstype ON properties_2017.buildingclasstypeid = buildingclasstype.buildingclasstypeid
-LEFT JOIN heatingorsystemtype ON properties_2017.heatingorsystemtypeid = heatingorsystemtype.heatingorsystemtypeid
-LEFT JOIN storytype ON properties_2017.storytypeid = storytype.storytypeid
-LEFT JOIN propertylandusetype ON properties_2017.propertylandusetypeid = propertylandusetype.propertylandusetypeid
-LEFT JOIN typeconstructiontype ON properties_2017.typeconstructiontypeid = typeconstructiontype.typeconstructiontypeid
-LEFT JOIN airconditioningtype ON properties_2017.airconditioningtypeid = airconditioningtype.airconditioningtypeid
-LEFT JOIN unique_properties ON properties_2017.parcelid = unique_properties.parcelid
-WHERE properties_2017.propertylandusetypeid = 261
-    AND transactiondate BETWEEN '2017-01-01' AND '2017-12-31'
+            SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, poolcnt, fips
+            FROM properties_2017
+            INNER JOIN predictions_2017 ON properties_2017.id = predictions_2017.id
+            WHERE propertylandusetypeid = 261 
+                AND transactiondate BETWEEN '2017-01-01' AND '2017-12-31'
                 """
     
     # Read in DataFrame from Codeup db.
@@ -41,12 +32,12 @@ WHERE properties_2017.propertylandusetypeid = 261
 
 def get_zillow_data():
     '''
-    This function checks if the 'sfr_2017.csv' file exists in a local file path. It it exists it will read the file into a pandas df. If the file does not exist, it will used the new_zillow_data() to read the zillow data from Codeup db into a df and cache the file as 'sfr_2017.csv' in the local repository. Returns df.
+    This function checks if the 'zillow.csv' file exists in a local file path. It it exists it will read the file into a pandas df. If the file does not exist, it will used the new_telco_data() to read the telco data from Codeup db into a df and cache the file as 'telco.csv' in the local repository. Returns df.
     '''
-    if os.path.isfile('sfr_2017.csv'):
+    if os.path.isfile('zillow.csv'):
         
         # If csv file exists read in data from csv file.
-        df = pd.read_csv('sfr_2017.csv', index_col=0)
+        df = pd.read_csv('zillow.csv', index_col=0)
         
     else:
         
@@ -54,6 +45,6 @@ def get_zillow_data():
         df = new_zillow_data()
         
         # Cache data
-        df.to_csv('sfr_2017.csv')
+        df.to_csv('zillow.csv')
         
     return df
--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@
 import warnings
 warnings.filterwarnings("ignore")
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import MinMaxScaler
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,24 +20,114 @@ def prep_zillow(df):
     ''' 
     This function takes in a dataframe, renames the columns, and drops nulls. Also, it changes datatypes for appropriate columns and renames fips to the actual county names. Returns cleaned df.
     '''
-    # drop duplicate columns and rows
+    # drop duplicates
     df = df.drop_duplicates()
-    df = df.T.drop_duplicates().T
-    df.rename(columns={'bathroomcnt' : 'baths',
-                       'bedroomcnt' : 'beds',
-                       'calculatedfinishedsquarefeet' : 'sqft',
-                       'taxvaluedollarcnt' : 'taxvalue',
-                       'fips' : 'county'}, inplace=True)
-    df.replace(0, np.nan, inplace=True)
-    df = df.dropna()
-    make_ints = ['beds','sqft','taxvalue','yearbuilt']
 
+    # rename columns
+    rename_columns ={
+        'bathroomcnt':'baths',
+        'bedroomcnt':'beds',
+        'calculatedfinishedsquarefeet':'sqft',
+        'fips':'county',
+        'taxvaluedollarcnt':'tax_value'}
+
+    df = df.rename(columns=rename_columns)
+
+    # handle nulls
+    df = df.fillna(0)
+    columns_to_replace = ['poolcnt']
+    df[columns_to_replace] = df[columns_to_replace].applymap(lambda x: 'No Pool' if x == 0 else x)
+    df = df[~(df == 0).any(axis=1)]
+    df[columns_to_replace] = df[columns_to_replace].applymap(lambda x: '0' if x == 'No Pool' else x)
+    
+    # correct dtypes
+    make_ints = ['beds','tax_value', 'yearbuilt', 'sqft', 'county', 'poolcnt']
+    make_float = ['baths']
+    
     for col in make_ints:
         df[col] = df[col].astype(int)
-        
-    # df.county = df.county.map({6037:'LA',6059:'Orange',6111:'Ventura'})
+    for col in make_float:
+        df[col] = df[col].astype(float)
+
+    # handle outliers (top and bottom 0.5 %)
+
+    # sort by tax_value
+    z_sorted = df.sort_values(by='tax_value', ascending=True)
+
+    # calculate the number of rows in the top and bottom 2%
+    num_rows = int(0.005 * len(z_sorted))
+
+    # remove the top and bottom .5% of rows
+    z_outliers_removed = z_sorted.iloc[num_rows:-num_rows]
+
+    # drop county
+    df = z_outliers_removed.drop(columns=['county'])
+    
     return df
 
+
+############################## PreProcess ZILLOW FUNCTION ##############################
+
+def preprocess(df):
+    
+    # split
+    train, validate, test = split_function(df)
+                                
+    # scale
+    train_scaled, validate_scaled, test_scaled = scale_data(train, validate, test)
+    
+    # split into x and y
+    x_train = train_scaled.drop(columns=['tax_value'])
+    y_train = train_scaled.tax_value
+
+    x_validate = validate_scaled.drop(columns=['tax_value'])
+    y_validate = validate_scaled.tax_value
+
+    x_test = test_scaled.drop(columns=['tax_value'])
+    y_test = test_scaled.tax_value
+    
+    return x_train, y_train, x_validate, y_validate, x_test, y_test, train_scaled, validate_scaled, test_scaled, train, validate, test
+
+
+############################ SCALE ZILLOW FUNCTION ############################
+
+# Scale
+def scale_data(train, 
+               validate, 
+               test, 
+               columns_to_scale=['beds', 'baths','sqft', 'poolcnt'],
+               return_scaler=False):
+    '''
+    Scales the 3 data splits. 
+    Takes in train, validate, and test data splits and returns their scaled counterparts.
+    If return_scalar is True, the scaler object will be returned as well
+    '''
+    # make copies of our original data so we dont gronk up anything
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    #     make the thing
+    scaler = MinMaxScaler()
+    #     fit the thing
+    scaler.fit(train[columns_to_scale])
+    # applying the scaler:
+    train_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(train[columns_to_scale]),
+                                                  columns=train[columns_to_scale].columns.values).set_index([train.index.values])
+                                                  
+    validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
+                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+    
+    test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
+                                                 columns=test[columns_to_scale].columns.values).set_index([test.index.values])
+    
+    if return_scaler:
+        return scaler, train_scaled, validate_scaled, test_scaled
+    else:
+        return train_scaled, validate_scaled, test_scaled
+
+############################## PREPARE ZILLOW FUNCTION ##############################
+
+    # df.county = df.county.map({6037:'LA',6059:'Orange',6111:'Ventura'})
 
 ############################ SPLIT ZILLOW FUNCTION ############################
 
